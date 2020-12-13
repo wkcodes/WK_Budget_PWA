4b8fc5d6d48ee7bda5e15ac90b13ff2e3643c826
Will Kendall <will@wills-MacBook-Pro.local>
2020-12-12 20:40:13 -0800

    configured mongo connection

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  ];
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+require('dotenv').config();
+
 const PORT = 3000;
 
 const app = express();
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.ATLAS_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
